eef2e9b3f584b2533801cd106250ad4d4f4a8781
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2022-08-10 16:44:05 -0300

    fixing search bar

--- src/Pages/Home/Location.jsx
+++ src/Pages/Home/Location.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import map from '../../assets/map.png'
 import {HiOutlineLocationMarker} from 'react-icons/hi'
-
+import {BsSearch} from 'react-icons/bs'
 export default function Location() {
   return (
     <div className='my-8'>
@@ -9,9 +9,12 @@ export default function Location() {
         <img src={map} className=""/>
 
         <div className='flex relative my-16 mx-auto md:w-[60%]'>
-            <HiOutlineLocationMarker className='absolute left-4 top-2' size={30} color="red"/>
-            <input type="text" placeholder='Busque aqui a sua localização' className= ' rounded-l-lg border bg-white-100 border-gray w-[600px] h-[45px] placeholder:pl-14 placeholder:font-mont placeholder:text-sm placeholder:sm:text-base placeholder:text-gray'/>
-            <button className='bg-red text-white-100 py-1  px-2 h-full w-[150px] rounded-r-lg absolute right-0'>Buscar</button>
+            <HiOutlineLocationMarker className='absolute left-4 top-2' size={24} color="red"/>
+            <input type="text" placeholder='Localização para entrega' className= ' rounded-lg border bg-white-100 border-gray w-[600px] h-[45px] placeholder:pl-14 placeholder:font-mont placeholder:text-xs placeholder:sm:text-base placeholder:text-gray'/>
+            <button className='bg-red text-white-100 py-1  px-2 h-full lg:w-[120px] w-[40px] rounded-r-lg absolute right-0'>
+              <span className='hidden lg:block'>Buscar</span>
+              <BsSearch className='lg:hidden block mx-auto' color='#FBFBFB' size={20}/>
+            </button>
         </div>
         </div>
   )
